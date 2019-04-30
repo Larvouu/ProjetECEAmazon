127c2670d50bb3bde15c59cd58acc018b34affaf
Busterfake <xelou1@hotmail.fr>
2019-04-30 15:23:15 +0200

    musique et images

--- cat_vet.php
+++ cat_vet.php
@@ -18,7 +18,7 @@
     <?php include 'navbar.php'; ?> 
 
 
-    <div class=" container-fluid" > 
+    <div class="container-fluid"> 
         <div class="overlay">
             <div class="description">
                 <h1>Nos musiques </h1>
@@ -34,10 +34,47 @@
             <div class="col-lg-4 col-md-4 col-sm-12">
             <h3 class="feature-title">Meilleure vente</h3>
             <img src="img/malte.jpg" class="img-fluid">
-            <p> Venez découvrir notre article le plus vendu et ne ratez pas votre chance de vous le procurer!</p>
+            <p> Artiste: Game of Thrones main theme <br>Prix: 1.29€ <br> durée: 1m51</p>
             <input  type="submit"  class="btn btn-secondary btn-block" formaction='assignment.php' value="J'en profite!" name="">
         </div>
+        <div class="col-lg-4 col-md-4 col-sm-12">
+            <h3 class="feature-title">Overgrown</h3>
+            <img src="img/wonder.jpg" class="img-fluid">
+            <p> Artiste: Oh Wonder <br>Prix: 1.19€ <br> durée: 3m58</p>
+            <input  type="submit"  class="btn btn-secondary btn-block" formaction='assignment.php' value="Acheter" name="">
         </div>
+        <div class="col-lg-4 col-md-4 col-sm-12">
+            <h3 class="feature-title">Pinacle</h3>
+            <img src="img/pinacle.jpg" class="img-fluid">
+            <p> Artiste: Lucio Bukowski <br>Prix: 0.99€ <br> durée: 7m35</p>
+            <input  type="submit"  class="btn btn-secondary btn-block" formaction='assignment.php' value="Acheter" name="">
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-12">
+            <h3 class="feature-title">What it takes</h3>
+            <img src="img/what_it_takes.jpg" class="img-fluid">
+            <p> Artiste: Aerosmith <br>Prix: 1.29€ <br> durée: 5m11</p>
+            <input  type="submit"  class="btn btn-secondary btn-block" formaction='assignment.php' value="Acheter" name="">
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-12">
+            <h3 class="feature-title">Happier</h3>
+            <img src="img/happier.jpg" class="img-fluid">
+            <p> Artiste: Marshmello <br>Prix: 1.39€ <br> Durée: 3m34</p>
+            <input  type="submit"  class="btn btn-secondary btn-block" formaction='assignment.php' value="Acheter" name="">
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-12">
+            <h3 class="feature-title">Let me down slowly</h3>
+            <img src="img/let_me_down.jpg" class="img-fluid">
+            <p> Artiste: Alec Benjamin <br>Prix: 1.15€ <br> durée: 1m49</p>
+            <input  type="submit"  class="btn btn-secondary btn-block" formaction='assignment.php' value="acheter" name="">
+        </div>
+        </div>
+        </div>
+
+        
+
+
+
+        
 
 
 
