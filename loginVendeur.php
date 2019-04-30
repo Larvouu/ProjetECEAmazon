c2b66adf233ebe43caa998919c4891746f3c7686
Schouketta <sarah.le@live.fr>
2019-04-30 14:54:17 +0200

    blindage connexion vendeur au top du top! :)

--- loginVendeur.php
+++ loginVendeur.php
@@ -3,48 +3,77 @@ echo "<head>
         <link href='loginVendeur.css' rel='stylesheet' type='text/css'>
     </head>";
 
+//Déclaration et initialisation des variables $email et $pseudo 
+//avec ce qui a été passé par méthode POST
+$email = isset($_POST["email"])? $_POST["email"] : "";
+$pseudo = isset($_POST["pseudo"])? $_POST["pseudo"] : "";
 
-if (isset($_POST["email"]))
+if ( $email=="" && $pseudo=="")
 {
-     //Déclaration et initialisation des variables $email et $pseudo 
-     //avec ce qui a été passé par méthode POST
-     $email = isset($_POST["email"])? $_POST["email"] : "";
-     $pseudo = isset($_POST["pseudo"])? $_POST["pseudo"] : "";
-
-     //Identifier la BDD
-     $database = "eceamazon";
-
-     //Connexion dans la BDD
-     $db_handle = mysqli_connect('localhost', 'root', '');
-     $db_found = mysqli_select_db($db_handle, $database);
-
-     if ($db_found) 
-    {
-        $sql = "SELECT email,pseudo FROM vendeur WHERE email = '$email' ";
-        $result = mysqli_query($db_handle, $sql);
-        
-        //regarder s'il y a de résultat
-        if (mysqli_num_rows($result) == 0) 
-        {
-            //l'email entré n'existe pas
-            echo "l'email n'a pas été trouvé dans la bdd";
-        } 
-        else 
-        {
-            //l'email entré a été trouvé 
-            while ($data = mysqli_fetch_assoc($result)) 
-            {
-                echo "email: " . $data['email'] . "<br>";
-                echo "pseudo: " . $data['pseudo'] . "<br>";
-                
-            }
-        }
-    }
-    else
-    {
-       echo "Sorry, Database not found";
-    }
+     echo "les champs n'ont pas été saisis!!!";
+    
 }
-mysqli_close($db_handle);
+else if ($email=="" && isset($_POST["pseudo"]))//si le champ email n'a pas été rempli
+{
+    
+    echo "<br><br><div class='bord'><br>";
+    echo "<p class='titre'>Le champ email est vide !</p></div>";
+    echo "<BR><br><div id='centrerB'><form><button id='submitB' type='submit' formaction='loginVendeurForm.php'>Ré-essayer de se créer un compte</button></div></form>";
 
+}
+else if($pseudo=="" && isset($_POST["email"]))//si le champ email n'a pas été rempli
+{
+    echo "<br><br><div class='bord'><br>";
+    echo "<p class='titre'>Le champ pseudo est vide !</p></div>";
+    echo "<BR><br><div id='centrerB'><form><button id='submitB' type='submit' formaction='loginVendeurForm.php'>Ré-essayer de se créer un compte</button></div></form>";
+}
+else //si les 2 valeurs ont ben été set
+{
+    //Identifier la BDD
+    $database = "eceamazon";
+
+    //Connexion dans la BDD
+    $db_handle = mysqli_connect('localhost', 'root', '');
+    $db_found = mysqli_select_db($db_handle, $database);
+
+    if ($db_found) 
+   {
+       $sql = "SELECT email,pseudo FROM vendeur WHERE email = '$email' ";
+       $result = mysqli_query($db_handle, $sql);
+       
+       //regarder s'il y a de résultat
+       if (mysqli_num_rows($result) == 0) 
+       {
+           //l'email entré n'existe pas
+           echo "l'email n'a pas été trouvé dans la bdd";
+       } 
+       else 
+       {
+           //l'email entré a été trouvé 
+           while ($data = mysqli_fetch_assoc($result)) 
+           {
+               echo "email: " . $data['email'] . "<br>";
+               echo "pseudo: " . $data['pseudo'] . "<br>";
+               
+           }
+       }
+    
+       mysqli_close($db_handle);
+   }
+   else
+   {
+      echo "Sorry, Database not found";
+   }
+
+}
+
+
+/*else if ($email=="" )//si le champ email n'a pas été rempli
+{
+    
+    echo "<br><br><div class='bord'><br>";
+    echo "<p class='titre'>Le champ email est vide !</p></div>";
+    echo "<BR><br><div id='centrerB'><form><button id='submitB' type='submit' formaction='loginVendeurForm.php'>Ré-essayer de se créer un compte</button></div></form>";
+
+}*/
 ?>
