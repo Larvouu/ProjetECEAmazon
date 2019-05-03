426e873e578812ceeb10dfeec0802d6ede051cce
Schouketta <sarah.le@live.fr>
2019-05-03 23:18:39 +0200

    accueil css

--- accueil.php
+++ accueil.php
@@ -37,16 +37,16 @@
   </ol>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block" src="img/c_music.jpg" style="width:70%; height:600px; border-radius:20px;" alt="First slide">
+      <img class="d-block" src="img/c_music.jpg" style="width:70%; height:700px; border-radius:20px;" alt="First slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block" src="img/c_sport.jpg" style="width:70%; height:600px; border-radius:20px;" alt="Second slide">
+      <img class="d-block" src="img/c_sport.jpg" style="width:70%; height:700px; border-radius:20px;" alt="Second slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block " src="img/c_clothes.jpg" style="width:70%; height:600px; border-radius:20px;" alt="Third slide">
+      <img class="d-block " src="img/c_clothes.jpg" style="width:70%; height:700px; border-radius:20px;" alt="Third slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block" src="img/c_book.jpeg" style="width:70%; height:600px; border-radius:20px;" alt="Fourth slide">
+      <img class="d-block" src="img/c_book.jpeg" style="width:70%; height:700px; border-radius:20px;" alt="Fourth slide">
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
