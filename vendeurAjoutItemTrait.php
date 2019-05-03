2710c299da378b462e0986e8a494c2ab692bce1d
Schouketta <sarah.le@live.fr>
2019-05-03 22:23:29 +0200

    ptite modif css

--- vendeurAjoutItemTrait.php
+++ vendeurAjoutItemTrait.php
@@ -26,27 +26,27 @@ if($categorie == "Musique")//nom(titre), auteur(artiste), descrip(genre), photo,
             <table style='margin-left:30px;'>
                 <tr>
                     <td><strong style='color:#ffe0ef; font-family: Ebrima; font-size: 19px;'>Titre de la musique :</strong></td>
-                    <td><input type='text' name='nom' placeholder='ex : Stand by Me'/></td>
+                    <td><input type='text'  class='inputText' name='nom' placeholder='ex : Stand by Me'/></td>
                 </tr>
 
                 <tr>
                     <td><strong style='color:#ffe0ef; font-family: Ebrima; font-size: 19px;'>Artiste :</strong></td>
-                    <td><input type='text' name='auteur' placeholder='ex : Ben E. King'/></td>
+                    <td><input type='text' class='inputText' name='auteur' placeholder='ex : Ben E. King'/></td>
                 </tr>
 
                 <tr>
                     <td><strong style='color:#ffe0ef; font-family: Ebrima; font-size: 19px;'>Genre :</strong></td>
-                    <td><input type='text' name='descrip' placeholder='ex : Soul'/></td>
+                    <td><input type='text' class='inputText' name='descrip' placeholder='ex : Soul'/></td>
                 </tr>
 
                 <tr>
                     <td><strong style='color:#ffe0ef; font-family: Ebrima; font-size: 19px;'>Lien photo :</strong></td>
-                    <td><input type='text' name='photo' placeholder='ex : img/nomPhoto.jpg'/></td>
+                    <td><input type='text' class='inputText' name='photo' placeholder='ex : img/nomPhoto.jpg'/></td>
                 </tr>
 
                 <tr>
                     <td><strong style='color:#ffe0ef; font-family: Ebrima; font-size: 19px;'>Prix :</strong></td>
-                    <td><input type='text' name='prix' placeholder='ex : 0.99&#8364'/></td>
+                    <td><input type='text' class='inputText' name='prix' placeholder='ex : 0.99&#8364'/></td>
                 </tr>
 
             </table>
@@ -68,27 +68,27 @@ else if ($categorie == "livre")//nom, auteur, descrip(description), photo,  prix
         <table style='margin-left:30px;'>
             <tr>
                 <td><strong style='color:#ffe0ef; font-family: Ebrima; font-size: 19px;'>Titre du livre : </strong></td>
-                <td><input type='text' name='nom' placeholder='ex : Les Misérables'/></td>
+                <td><input type='text' class='inputText' name='nom' placeholder='ex : Les Misérables'/></td>
             </tr>
 
             <tr>
                 <td><strong style='color:#ffe0ef; font-family: Ebrima; font-size: 19px;'>Auteur : </strong></td>
-                <td><input type='text' name='auteur' placeholder='ex : Victor Hugo'/></td>
+                <td><input type='text' class='inputText' name='auteur' placeholder='ex : Victor Hugo'/></td>
             </tr>
 
             <tr>
                 <td><strong style='color:#ffe0ef; font-family: Ebrima; font-size: 19px;'>Prix :</strong></td>
-                <td><input type='text' name='prix' placeholder='ex : 4,49&#8364'/></td>
+                <td><input type='text' class='inputText' name='prix' placeholder='ex : 4,49&#8364'/></td>
             </tr>
 
             <tr>
                 <td><strong style='color:#ffe0ef; font-family: Ebrima; font-size: 19px;'>Date de parution : </strong></td>
-                <td><input type='text' name='descrip' placeholder='ex : 1862'/></td>
+                <td><input type='text' class='inputText' name='descrip' placeholder='ex : 1862'/></td>
             </tr>
 
             <tr>
                 <td><strong style='color:#ffe0ef; font-family: Ebrima; font-size: 19px;'>Page de couverture:</strong></td>
-                <td><input type='text' name='photo' placeholder='ex : img/nomImage.jpg'/></td>
+                <td><input type='text' class='inputText' name='photo' placeholder='ex : img/nomImage.jpg'/></td>
             </tr>
 
         </table>
@@ -111,22 +111,22 @@ else if ($categorie == "sport")//nom, photo, descrip(descrip + durée), prix, --
         <table style='margin-left:30px;'>
             <tr>
                 <td><strong style='color:#ffe0ef; font-family: Ebrima; font-size: 19px;'>Nom de l'activité :</strong></td>
-                <td><input type='text' name='nom' placeholder='ex : EuropaPark (PASS 1 DAY)'/></td>
+                <td><input type='text' class='inputText' name='nom' placeholder='ex : EuropaPark (PASS 1 DAY)'/></td>
             </tr>
 
             <tr>
                 <td><strong style='color:#ffe0ef; font-family: Ebrima; font-size: 19px;'>Description :</strong></td>
-                <td><input type='text' name='descrip' placeholder='ex : Meilleur parc d'attraction'/></td>
+                <td><input type='text' class='inputText' name='descrip' placeholder='ex : Meilleur parc d'attraction'/></td>
             </tr>
 
             <tr>
                 <td><strong style='color:#ffe0ef; font-family: Ebrima; font-size: 19px;'>Prix :</strong></td>
-                <td><input type='text' name='prix' placeholder='ex : 54.00&#8364'/></td>
+                <td><input type='text' class='inputText' name='prix' placeholder='ex : 54.00&#8364'/></td>
             </tr>
 
             <tr>
                 <td><strong style='color:#ffe0ef; font-family: Ebrima; font-size: 19px;'>Photo de l'activité :</strong></td>
-                <td><input type='text' name='photo' placeholder='ex : img/nomImage.jpg'/></td>
+                <td><input type='text' class='inputText' name='photo' placeholder='ex : img/nomImage.jpg'/></td>
             </tr>
 
         </table>
@@ -148,31 +148,31 @@ else if ($categorie == "teeshirt")//nom, descrip, photo, prix, tailleS, tailleM,
         <table style='margin-left:30px;'>
             <tr>
                 <td><strong style='color:#ffe0ef; font-family: Ebrima; font-size: 19px;'>Nom du TeeShirt :</strong></td>
-                <td><input type='text' name='nom' placeholder='ex : TeeShirt Calvin Klein'/></td>
+                <td><input type='text'  class='inputText' name='nom' placeholder='ex : TeeShirt Calvin Klein'/></td>
             </tr>
 
             <tr>
                 <td><strong style='color:#ffe0ef; font-family: Ebrima; font-size: 19px;'>Description :</strong></td>
-                <td><input type='text' name='descrip' placeholder='ex : col rond, cintré'/></td>
+                <td><input type='text' class='inputText' name='descrip' placeholder='ex : col rond, cintré'/></td>
             </tr>
 
             <tr>
                 <td><strong style='color:#ffe0ef; font-family: Ebrima; font-size: 19px;'>Photo du TeeShirt:</strong></td>
-                <td><input type='text' name='photo' placeholder='ex : img/nomPhoto.jpg'/></td>
+                <td><input type='text' class='inputText' name='photo' placeholder='ex : img/nomPhoto.jpg'/></td>
             </tr>
 
             <tr>
                 <td><strong style='color:#ffe0ef; font-family: Ebrima; font-size: 19px;'>Prix :</strong></td>
-                <td><input type='text' name='prix' placeholder='ex : 36.50&#8364'/></td>
+                <td><input type='text' class='inputText' name='prix' placeholder='ex : 36.50&#8364'/></td>
             </tr>
 
             <tr>
                 <td><strong style='color:#ffe0ef; font-family: Ebrima; font-size: 19px;'>Tailles disponibles: </strong></td>
                 
             </tr>
-        </table><input type='text' name='tailleS' placeholder='ex : S'/><br>
-                <input type='text' name='tailleM' placeholder='ex : M'/><br>
-                <input type='text' name='tailleL' placeholder='ex : L'/>
+        </table><input type='text' class='inputTaille' name='tailleS' placeholder='ex : S'/><br>
+                <input type='text' class='inputTaille' name='tailleM' placeholder='ex : M'/><br>
+                <input type='text' class='inputTaille' name='tailleL' placeholder='ex : L'/>
             <br><br>
             <input class='button' type='submit' name='submit' value='ENTER'/>
     
@@ -191,31 +191,31 @@ else if ($categorie == "chaussh")//nom, descrip, photo, prix, tailleCh1, tailleC
         <table style='margin-left:30px;'>
             <tr>
                 <td><strong style='color:#ffe0ef; font-family: Ebrima; font-size: 19px;'>Nom de la chaussure :</strong></td>
-                <td><input type='text' name='nom' placeholder='ex : Nike SB Janoski'/></td>
+                <td><input type='text' class='inputText' name='nom' placeholder='ex : Nike SB Janoski'/></td>
             </tr>
 
             <tr>
                 <td><strong style='color:#ffe0ef; font-family: Ebrima; font-size: 19px;'>Description :</strong></td>
-                <td><input type='text' name='descrip' placeholder='ex : for daily use or skateboard'/></td>
+                <td><input type='text' class='inputText' name='descrip' placeholder='ex : for daily use or skateboard'/></td>
             </tr>
 
             <tr>
                 <td><strong style='color:#ffe0ef; font-family: Ebrima; font-size: 19px;'>Lien photo :</strong></td>
-                <td><input type='text' name='photo' placeholder='ex : img/nomPhoto.jpg'/></td>
+                <td><input type='text' class='inputText' name='photo' placeholder='ex : img/nomPhoto.jpg'/></td>
             </tr>
 
             <tr>
                 <td><strong style='color:#ffe0ef; font-family: Ebrima; font-size: 19px;'>Prix :</strong></td>
-                <td><input type='text' name='prix' placeholder='ex : 72.00&#8364'/></td>
+                <td><input type='text' class='inputText' name='prix' placeholder='ex : 72.00&#8364'/></td>
             </tr>
 
             <tr>
                 <td><strong style='color:#ffe0ef; font-family: Ebrima; font-size: 19px;'>Tailles disponibles :</strong></td>
             </tr>
         </table>
-                <input type='text' name='tailleCh1' placeholder='ex : 41'/><br>
-                <input type='text' name='tailleCh2' placeholder='ex : 42'/><br>
-                <input type='text' name='tailleCh3' placeholder='ex : 44'/>
+                <input type='text' class='inputTaille' name='tailleCh1' placeholder='ex : 41'/><br>
+                <input type='text' class='inputTaille' name='tailleCh2' placeholder='ex : 42'/><br>
+                <input type='text' class='inputTaille' name='tailleCh3' placeholder='ex : 44'/>
 
             <br><br>
             <input class='button' type='submit' name='submit' value='ENTER'/>
@@ -235,38 +235,38 @@ else if ($categorie == "chaussf")//nom, descrip, photo, prix, tailleCh1, tailleC
         <table style='margin-left:30px;'>
             <tr>
                 <td><strong style='color:#ffe0ef; font-family: Ebrima; font-size: 19px;'>Nom de la chaussure :</strong></td>
-                <td><input type='text' name='nom' placeholder='ex : Bottines talons Minelli '/></td>
+                <td><input type='text' class='inputText' name='nom' placeholder='ex : Bottines talons Minelli '/></td>
             </tr>
 
             <tr>
                 <td><strong style='color:#ffe0ef; font-family: Ebrima; font-size: 19px;'>Description :</strong></td>
-                <td><input type='text' name='descrip' placeholder='ex : faux-leather noir'/></td>
+                <td><input type='text' class='inputText' name='descrip' placeholder='ex : faux-leather noir'/></td>
             </tr>
 
             <tr>
                 <td><strong style='color:#ffe0ef; font-family: Ebrima; font-size: 19px;'>Lien photo :</strong></td>
-                <td><input type='text' name='photo' placeholder='ex : img/nomPhoto.jpg'/></td>
+                <td><input type='text' class='inputText' name='photo' placeholder='ex : img/nomPhoto.jpg'/></td>
             </tr>
 
             <tr>
                 <td><strong style='color:#ffe0ef; font-family: Ebrima; font-size: 19px;'>Prix :</strong></td>
-                <td><input type='text' name='prix' placeholder='ex : 72.00&#8364'/></td>
+                <td><input type='text' class='inputText' name='prix' placeholder='ex : 72.00&#8364'/></td>
             </tr>
 
             <tr>
                 <td><strong style='color:#ffe0ef; font-family: Ebrima; font-size: 19px;'>Tailles disponibles :</strong></td>
             </tr>
         </table>
-                <input type='text' name='tailleCh1' placeholder='ex : 36'/><br>
-                <input type='text' name='tailleCh2' placeholder='ex : 37'/><br>
-                <input type='text' name='tailleCh3' placeholder='ex : 38'/><br>
+                <input type='text' class='inputTaille' name='tailleCh1' placeholder='ex : 36'/><br>
+                <input type='text' class='inputTaille' name='tailleCh2' placeholder='ex : 37'/><br>
+                <input type='text' class='inputTaille' name='tailleCh3' placeholder='ex : 38'/><br>
         <table>
             <tr>
             <td><strong style='color:#ffe0ef; font-family: Ebrima; font-size: 19px;'>Couleurs disponibles :</strong></td>
             </tr>
         </table>
-                <input type='text' name='couleur1' placeholder='ex : orange'/><br>
-                <input type='text' name='couleur2' placeholder='ex : violet'/><br>
+                <input type='text' class='inputTaille' name='couleur1' placeholder='ex : orange'/><br>
+                <input type='text' class='inputTaille' name='couleur2' placeholder='ex : violet'/><br>
 
         <br><br>
         <input class='button' type='submit' name='submit' value='ENTER'/>
