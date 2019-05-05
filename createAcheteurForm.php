4547d638b96c6a956854500ef151d3902e4d4267
Schouketta <sarah.le@live.fr>
2019-05-05 18:18:00 +0200

    Toutes les pages LOGIN CSS épuré2

--- createAcheteurForm.php
+++ createAcheteurForm.php
@@ -7,49 +7,70 @@
     
     <?php include 'navbar.php'; ?>
    
-        <div class="uploadImgUser">
-            <strong>Compléter ces champs afin de vous créer un compte Acheteur :</strong>
+
+<div class="logForm">
+        <br><br>
+        <p id="loginInterface">Login</p><br>
+       
+        <form action="loginVendeur.php" method="post">
+            <div class="form-group">
+                <label for="exampleInputEmail1">Email</label>
+                <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Saisir email">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Pseudo</label>
+                <input type="text" name="pseudo" class="form-control" id="exampleInputPassword1" placeholder="Pseudo">
+            </div>
+            <br>
+            <input id="button" type="submit" value="ENTER" name='connect'>
+        </form>
+    </div>
+
+
+        <div class="logForm">
+            <br>
+            <p id="loginInterface">Création d'un nouveau compte <strong>Client</strong></p><br>
+
             <form action="createAcheteur.php" method="post">
-                
             <table style="margin-left:30px;">
                 <tr>
-                    <td><strong style="color:#ffe0ef; font-family: Ebrima; font-size: 19px;">Email :</strong></td>
+                    <td style="color:white; font-family: Ebrima; font-size: 19px;">Email :</td>
                     <td><input type="text" name="email"/></td>
                 </tr>
                 <tr>
-                    <td><strong style="color:#ffe0ef; font-family: Ebrima; font-size: 19px;">Mot de passe :</strong></td>
+                    <td style="color:white; font-family: Ebrima; font-size: 19px;">Mot de passe :</td>
                     <td><input type="password" name="mdp"/></td>
                 </tr>
                 <tr>
-                    <td><strong style="color:#ffe0ef; font-family: Ebrima; font-size: 19px;">Nom :</strong></td>
+                    <td style="color:white; font-family: Ebrima; font-size: 19px;">Nom :</td>
                     <td><input type="text" name="nom"/></td>
                 </tr>
                 <tr>
-                    <td><strong style="color:#ffe0ef; font-family: Ebrima; font-size: 19px;">Prénom :</strong></td>
+                    <td style="color:white; font-family: Ebrima; font-size: 19px;">Prénom :</td>
                     <td><input type="text" name="prenom"/></td>
                 </tr>
                 <tr>
-                    <td><strong style="color:#ffe0ef; font-family: Ebrima; font-size: 19px;">Ville :</strong></td>
+                    <td style="color:white; font-family: Ebrima; font-size: 19px;">Ville :</td>
                     <td><input type="text" name="ville"/></td>
                 </tr>
                 <tr>
-                    <td><strong style="color:#ffe0ef; font-family: Ebrima; font-size: 19px;">Adresse :</strong></td>
+                    <td style="color:white; font-family: Ebrima; font-size: 19px;">Adresse :</td>
                     <td><input type="text" name="adresse"/></td>
                 </tr>
                 <tr>
-                    <td><strong style="color:#ffe0ef; font-family: Ebrima; font-size: 19px;">Code postal :</strong></td>
+                    <td style="color:white; font-family: Ebrima; font-size: 19px;">Code postal :</td>
                     <td><input type="text" name="codePostal"/></td>
                 </tr>
                 <tr>
-                    <td><strong style="color:#ffe0ef; font-family: Ebrima; font-size: 19px;">Pays :</strong></td>
+                    <td style="color:white; font-family: Ebrima; font-size: 19px;">Pays :</td>
                     <td><input type="text" name="pays"/></td>
                 </tr>
                 <tr>
-                    <td><strong style="color:#ffe0ef; font-family: Ebrima; font-size: 19px;">Numéro de téléphone :</strong></td>
+                    <td style="color:white; font-family: Ebrima; font-size: 19px;">Numéro de téléphone :</td>
                     <td><input type="text" name="numTel"/></td>
                 </tr>
                 <tr>
-                    <td><strong style="color:#ffe0ef; font-family: Ebrima; font-size: 19px;">Numéro de carte :</strong></td>
+                    <td><strong style="color:#ffe0ef; font-family: Ebrima; font-size: 19px;">Numéro de carte :</td>
                     <td><input type="password" name="numCarte"/></td>
                 </tr>
                 <tr>
@@ -71,7 +92,7 @@
 
             <table>
                 <br><br>
-                <input class="button" type="submit" name="submit" value="ENTER"/>
+                <input id="button" type="submit" name="submit" value="ENTER"/>
         </div>
     </form>
     
