8f2bee23956d3fdae7c1b69ff4050b661ceda26c
Schouketta <sarah.le@live.fr>
2019-05-05 15:53:56 +0200

    Footer accueil

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <!-- Footer pris sur le site suivant https://mdbootstrap.com/docs/jquery/navigation/footer/ -->
+<hr>
     <footer class="page-footer font-small mdb-color pt-4">
 
 <!-- Footer Links -->
@@ -10,7 +11,7 @@
     <!-- Grid column -->
     <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
       <h6 class="text-uppercase mb-4 font-weight-bold">ECE Amazon</h6>
-      <p>ECE Amazon est un site de e-commerce. Vous trouverez une panoplie de produits, en passant par les livres </p>
+      <p>ECE Amazon est un site de e-commerce. Vous trouverez une panoplie de produits pour tous les goûts. </p>
     </div>
     <!-- Grid column -->
 
@@ -18,18 +19,18 @@
 
     <!-- Grid column -->
     <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
-      <h6 class="text-uppercase mb-4 font-weight-bold">Products</h6>
+      <h6 class="text-uppercase mb-4 font-weight-bold">Produits</h6>
       <p>
-        <a href="#!">MDBootstrap</a>
+        <a style="color:#7158e2;" href="cat_musique.php">Musique</a>
       </p>
       <p>
-        <a href="#!">MDWordPress</a>
+        <a style="color:#7158e2;" href="cat_livre.php">Livres</a>
       </p>
       <p>
-        <a href="#!">BrandFlow</a>
+        <a style="color:#7158e2;" href="cat_sport.php">Sports et Loisirs</a>
       </p>
       <p>
-        <a href="#!">Bootstrap Angular</a>
+        <a style="color:#7158e2;" href="chaussures_h.php">Chaussures</a>
       </p>
     </div>
     <!-- Grid column -->
@@ -38,18 +39,18 @@
 
     <!-- Grid column -->
     <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
-      <h6 class="text-uppercase mb-4 font-weight-bold">Useful links</h6>
+      <h6 class="text-uppercase mb-4 font-weight-bold">Liens utils</h6>
       <p>
-        <a href="#!">Your Account</a>
+        <a style="color:#7158e2;" href="ventes_flash.php">Ventes Flash</a>
       </p>
       <p>
-        <a href="#!">Become an Affiliate</a>
+        <a style="color:#7158e2;" href="vendeurFormulaire.php">Vendre un produit</a>
       </p>
       <p>
-        <a href="#!">Shipping Rates</a>
+        <a style="color:#7158e2;" href="admin.php">Admin</a>
       </p>
       <p>
-        <a href="#!">Help</a>
+        <a style="color:#7158e2;" href="#!">Help</a>
       </p>
     </div>
 
@@ -60,13 +61,13 @@
     <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
       <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
       <p>
-        <i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
+        <i class="fas fa-home mr-3"></i> ECE Paris, Paris, 75015</p>
       <p>
-        <i class="fas fa-envelope mr-3"></i> info@gmail.com</p>
+        <i class="fas fa-envelope mr-3"></i> sarah.le@edu.ece.fr</p>
       <p>
-        <i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
+        <i class="fas fa-phone mr-3"></i> antoine-babak@edu.ece.fr</p>
       <p>
-        <i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+        <i class="fas fa-print mr-3"></i> axel.vinant@edu.ece.fr</p>
     </div>
     <!-- Grid column -->
 
@@ -83,42 +84,10 @@
 
       <!--Copyright-->
       <p class="text-center text-md-left">© 2018 Copyright:
-        <a href="https://mdbootstrap.com/education/bootstrap/">
-          <strong> MDBootstrap.com</strong>
+        <a style="color:#7158e2;" href="accueil.php">
+          <strong> ECEAmazon.com</strong>
         </a>
       </p>
-
-    </div>
-    <!-- Grid column -->
-
-    <!-- Grid column -->
-    <div class="col-md-5 col-lg-4 ml-lg-0">
-
-      <!-- Social buttons -->
-      <div class="text-center text-md-right">
-        <ul class="list-unstyled list-inline">
-          <li class="list-inline-item">
-            <a class="btn-floating btn-sm rgba-white-slight mx-1">
-              <i class="fab fa-facebook-f"></i>
-            </a>
-          </li>
-          <li class="list-inline-item">
-            <a class="btn-floating btn-sm rgba-white-slight mx-1">
-              <i class="fab fa-twitter"></i>
-            </a>
-          </li>
-          <li class="list-inline-item">
-            <a class="btn-floating btn-sm rgba-white-slight mx-1">
-              <i class="fab fa-google-plus-g"></i>
-            </a>
-          </li>
-          <li class="list-inline-item">
-            <a class="btn-floating btn-sm rgba-white-slight mx-1">
-              <i class="fab fa-linkedin-in"></i>
-            </a>
-          </li>
-        </ul>
-      </div>
 
     </div>
     <!-- Grid column -->
